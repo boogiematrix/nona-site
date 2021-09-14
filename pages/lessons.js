d12433a49e93508d124fe41e8437dc01972907be
Night Chough <chicken.snuggles@gmail.com>
2021-09-14 13:54:03 -0500

    added components

--- pages/lessons.js
+++ pages/lessons.js
@@ -1,12 +1,14 @@
 import Head from 'next/head'
-
+import Layout from '../components/layout'
 export default function Lessons() {
     return (
         <>
             <Head>
                 <title>Piano Lessons</title>
             </Head>
-            <h1>Piano Lessons</h1>
+            <Layout>
+                <h1>Piano Lessons</h1>
+            </Layout>
         </>
     )
 }
