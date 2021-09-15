f09942d9eda6058987a9eeea1f57486c9561a125
Night Chough <chicken.snuggles@gmail.com>
2021-09-15 10:53:44 -0500

    added nav

---
+++ components/nav.js
@@ -0,0 +1,15 @@
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+export default function Nav() {
+    return (
+        <nav>
+            <Link href=''><a></a></Link>
+            <Link href=''><a></a></Link>
+            <Link href=''><a></a></Link>
+            <Link href=''><a></a></Link>
+            <Link href=''><a></a></Link>
+        </nav>
+    )
+}
