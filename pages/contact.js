7f968f1d19fdb3ccceaa8c51739798fea3c5e4cb
Night Chough <chicken.snuggles@gmail.com>
2021-09-18 12:44:55 -0500

    basic html for contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,38 @@
 import Head from 'next/head'
+import Layout from '../components/layout'
 
 export default function Contact() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>Contact</title>
             </Head>
-            <h1>Contact</h1>
-        </>
+            <div>
+                <h1>Contact</h1>
+                <p>Keep in touch! Let me know if you are interested in real estate news or music and art updates or videos of my animals being wild and I will periodically send out some info.
+                    <br/>
+                    nonainvie@gmail.com
+                    <br/>
+                    <a href='https://linktr.ee/nonainvie'>Mutual Aid and Community Care Links</a>
+                </p>
+            </div>
+            <form>
+                <label>Name *</label>
+                <div>
+                    <div>
+                        <input></input>
+                        <label>first name</label>
+                    </div>
+                    <div>
+                        <input></input>
+                        <label>last name</label>
+                    </div>
+                </div>
+                <label>Email *</label>
+                <input></input>
+                <label>Message</label>
+                <textarea></textarea>
+            </form>
+        </Layout>
     )
 }
